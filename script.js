ae04e1f3f4fc4121c83a19d08561e2f32a19afc2
Franco Navarro <franco11navarro11@gmail.com>
2022-08-02 16:05:19 -0300

    second commit

--- script.js
+++ script.js
@@ -1,71 +1,100 @@
-const $beer = document.getElementById("beer"),
-$menu = document.getElementById("menu"),
-$close = document.getElementById("close"),
-$menuCont = document.getElementById("menu-list"),
-$rubia = document.getElementById("rubia"),
-$roja = document.getElementById("roja"),
-$blend = document.getElementById("blend");
 
+//Loading modal
+((d)=> {
+    d.addEventListener("DOMContentLoaded", ()=>{
+        setTimeout(()=>{
+            d.getElementById("load").classList.add("none");
+            d.getElementById("body").style.overflowY = "auto";
+        },3000);
+    });
+})(document);
 
+//Background scroll
 window.addEventListener("scroll", ()=> {
-    /*let value = window.scrollY;
+    const $beer = document.getElementById("beer"),
+    $title = document.getElementById("title"),
+    $back1 = document.getElementById("back-1"),
+    $back2 = document.getElementById("back-2"),
+    $back3 = document.getElementById("back-3"),
+    $back4 = document.getElementById("back-4");
+    let value = window.scrollY;
+    $beer.style.top = value * 0.2 + "px";
+    $title.style.top = value * 0.4 + "px";
+    $title.style.left = value * 0.2 + "px";
+    $back1.style.width = 100 + (value * 0.6) + "%";
+    $back2.style.width = 100 + (value * 0.6) + "%";
+    $back3.style.width = 100 + (value * 0.6) + "%";
+    $back4.style.width = 100 + (value * 0.6) + "%";
+});
+
+//Menu desplegable
+((d)=> {
+    const $menu = d.getElementById("menu"),
+    $close = d.getElementById("close"),
+    $menuCont = d.getElementById("menu-list"),
+    $burro = d.getElementById("burro");
+
+    $menu.addEventListener("click", ()=> {
+        $menuCont.classList.add("header-nav-list-center");
+        $burro.classList.add("header-nav-list-center");
+        $menu.classList.add("none");
+        $close.classList.remove("none");
+        
     
-    $beer.style.top = value * 0.6 + "px";*/
-})
-
-$menu.addEventListener("click", ()=> {
-    $menuCont.classList.add("header-nav-list-center");
-    $menu.classList.add("none");
-    $close.classList.remove("none");
-
-    document.addEventListener("click", e=>{
-        if(!e.target.matches(".header-nav-list-a a")) return false;
-
+        d.addEventListener("click", e=>{
+            if(!e.target.matches(".header-nav-list-a a")) return false;
+            $menuCont.classList.remove("header-nav-list-center");
+            $burro.classList.remove("header-nav-list-center");
+            $menu.classList.remove("none");
+            $close.classList.add("none");
+        })
+    })
+    
+    $close.addEventListener("click", ()=> {
         $menuCont.classList.remove("header-nav-list-center");
+        $burro.classList.remove("header-nav-list-center");
         $menu.classList.remove("none");
         $close.classList.add("none");
     })
-})
+})(document);
 
-$close.addEventListener("click", ()=> {
-    $menuCont.classList.remove("header-nav-list-center");
-    $menu.classList.remove("none");
-    $close.classList.add("none");
-})
+//Contenido desplegables 
+((d)=> {
+    const $rubia = d.getElementById("rubia"),
+    $roja = d.getElementById("roja"),
+    $blend = d.getElementById("blend"),
+    $contain = d.getElementById("beer-contain");
 
+    $rubia.addEventListener("click", ()=> {
+        if(window.innerWidth >= 1024) {
+            $contain.classList.toggle("beer-present-extend");
+            $blend.classList.toggle("none");
+            $roja.classList.toggle("none");
+            $contain.classList.toggle("beer-present");
+            $blend.classList.toggle("beer");
+            $roja.classList.toggle("beer");
+        }
+    })
 
-$rubia.addEventListener("click", ()=> {
-    if(window.innerWidth >= 1024) {
-        const $contain = document.getElementById("beer-contain");
-        $contain.classList.toggle("beer-present-extend");
-        $blend.classList.toggle("none");
-        $roja.classList.toggle("none");
-        $contain.classList.toggle("beer-present");
-        $blend.classList.toggle("beer");
-        $roja.classList.toggle("beer");
-    }
-})
+    $roja.addEventListener("click", ()=> {
+        if(window.innerWidth >= 1024) {
+            $contain.classList.toggle("beer-present");
+            $blend.classList.toggle("beer");
+            $rubia.classList.toggle("beer");
+            $contain.classList.toggle("beer-present-extend");
+            $blend.classList.toggle("none");
+            $rubia.classList.toggle("none");
+        }
+    })
 
-$roja.addEventListener("click", ()=> {
-    if(window.innerWidth >= 1024) {
-        const $contain = document.getElementById("beer-contain");
-        $contain.classList.toggle("beer-present");
-        $blend.classList.toggle("beer");
-        $rubia.classList.toggle("beer");
-        $contain.classList.toggle("beer-present-extend");
-        $blend.classList.toggle("none");
-        $rubia.classList.toggle("none");
-    }
-})
-
-$blend.addEventListener("click", ()=> {
-    if(window.innerWidth >= 1024) {
-        const $contain = document.getElementById("beer-contain");
-        $contain.classList.toggle("beer-present-extend");
-        $rubia.classList.toggle("none");
-        $roja.classList.toggle("none");
-        $contain.classList.toggle("beer-present");
-        $rubia.classList.toggle("beer");
-        $roja.classList.toggle("beer");
-    }
-})
+    $blend.addEventListener("click", ()=> {
+        if(window.innerWidth >= 1024) {
+            $contain.classList.toggle("beer-present-extend");
+            $rubia.classList.toggle("none");
+            $roja.classList.toggle("none");
+            $contain.classList.toggle("beer-present");
+            $rubia.classList.toggle("beer");
+            $roja.classList.toggle("beer");
+        }
+    })
+})(document);
